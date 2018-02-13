ebcda59460a4fa9b59b57306130cba62a9065e9e
Michael Murad <neverthegreat@icloud.com>
2018-02-12 18:39:26 -0800

    Added promisify moved default export to top

--- src/rateLimiter/overdrive.ts
+++ src/rateLimiter/overdrive.ts
@@ -2,57 +2,57 @@ import { RedisOptions, Redis as RedisClient } from "ioredis";
 import { NextFunction, Response } from "express";
 import { promisify } from "util";
 
-const overdrive = (
+export default (
   rateId: string,
   redis: RedisClient,
   rateLimit: number,
   expireMilisecs: number,
-  increaseByInc: number,
   res: Response,
-  next: Function
+  next: NextFunction
 ) => {
   const getAsync = promisify(redis.get).bind(redis);
-  // return redis.options["socket_nodelay"] === undefined ? redis.get(rateId) : getAsync(rateId)
-  return redis
-    .getAsync(rateId)
+  const psetexAsync = promisify(redis.psetex).bind(redis);
+
+  return getAsync(rateId)
     .then((score: string) => {
       const rateScore: number = parseInt(score, 10) || 1;
       console.log("get result: ", rateScore);
-      // if the value is x amount greater than the limit
-      // this will block the action for quite some time and also stop increasing the value
-      // and reset the expire time
+      // if the calue is over 10x the value it will simply block it
       if (rateScore > rateLimit * 10) {
-        return redis
-          .psetex(rateId, expireMilisecs * 1000, rateScore)
-          .then(() => {
+        return res.status(403).send({
+          error: "You are doing this WAY too much, try again much later"
+        });
+      }
+      // if the value is 10x the limit
+      // this will block the action for 1000x the expire time
+      if (rateScore === rateLimit * 10) {
+        return psetexAsync(rateId, expireMilisecs * 1000, rateScore + 1).then(
+          () => {
             res.status(403).send({
-              error: "You are doing this WAY too much, try again in a few hours"
+              error: "You are doing this WAY too much, try again much later"
             });
             return res.end();
-          });
+          }
+        );
       }
       // otherwise this will block the action for a short time and still increment the value
       // and reset the expire time
       if (rateScore > rateLimit) {
-        return redis
-          .psetex(rateId, expireMilisecs, increaseByInc + rateScore)
-          .then(() => {
-            res.status(403).send({
-              error: "You are doing this too much, try again in a few minutes"
-            });
-            return res.end();
+        return psetexAsync(rateId, expireMilisecs, 1 + rateScore).then(() => {
+          res.status(403).send({
+            error: "You are doing this too much, try again in a little bit"
           });
+          return res.end();
+        });
       }
       // if the value isn't out of limit then allow action,
       // increment value, and reset expiration time
-      return redis
-        .psetex(rateId, expireMilisecs, increaseByInc + rateScore)
-        .then(() => next());
+      return psetexAsync(rateId, expireMilisecs, 1 + rateScore).then(() =>
+        next()
+      );
     })
     .catch(err => {
       res.status(500).send(err);
       return res.end();
     });
 };
-
-export default overdrive;
