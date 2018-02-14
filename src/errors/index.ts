d458d60536328ee8b38d510b9ac9cc8796b30893
Michael Murad <neverthegreat@icloud.com>
2018-02-13 23:03:57 -0800

    Added errors module

---
+++ src/errors/index.ts
@@ -0,0 +1,7 @@
+export const rateError: Object = {
+  error: "You are doing this too much, try again in a little bit"
+};
+
+export const overdriveRateErr: Object = {
+  error: "You are doing this WAY too much, try again much later"
+};
