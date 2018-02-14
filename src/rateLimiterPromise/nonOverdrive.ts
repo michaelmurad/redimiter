e13c300f1e237e05aebe73b0e8f87647b6cb95e6
Michael Murad <neverthegreat@icloud.com>
2018-02-14 11:19:40 -0800

    Added comments

--- src/rateLimiterPromise/nonOverdrive.ts
+++ src/rateLimiterPromise/nonOverdrive.ts
@@ -12,14 +12,16 @@ export default (
   resolve: Function
 ) => {
   const request = "r";
-
+  // gets the list score
   return redis.llen(rateId, (err, score) => {
     if (err) {
       return reject(err);
     }
+    // rate limiter kicks into action
     if (score >= limit) {
       return reject(rateError);
     }
+    // if there is no score it creates one and sets expire
     if (!score) {
       return redis
         .multi()
@@ -32,6 +34,8 @@ export default (
           return resolve(true);
         });
     }
+    // if it exists and is below the limit it ill add an item
+    // and increase the score
     return redis.rpushx(rateId, request, (rpushErr, _) => {
       if (rpushErr) {
         return reject(rpushErr);
