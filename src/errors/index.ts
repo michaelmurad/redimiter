cd697b16cd58b1ba775355f852cfd51c36a4818b
Michael Murad <neverthegreat@icloud.com>
2019-07-19 12:02:19 -0700

    updated errors

--- src/errors/index.ts
+++ src/errors/index.ts
@@ -1,44 +1,43 @@
-function ErrorGen(message: string, name: string = "Error") {
-  this.name = name;
-  this.message = message;
-}
+export const rateError = {
+  name: "Error",
+  message: "You are doing this too much, try again in a little bit"
+};
 
-export const rateError: Error = new ErrorGen(
-  "You are doing this too much, try again in a little bit"
-);
-
-export const overdriveRateErr: Error = new ErrorGen(
-  "You are doing this WAY too much, try again much later"
-);
+export const overdriveRateErr = {
+  name: "Error",
+  message: "You are doing this WAY too much, try again much later"
+};
 
 // CONSTRUCTOR ERRORS
-export const constrErr: Error = new ErrorGen("You need to add a redis client");
+export const constrErr: Error = new Error("You need to add a redis client");
 
 // RATE_LIMITER ERRORS
-export const rLPathErr: string = "rateLimiter({ path }) must be a string";
+export const rLPathErr: Error = new Error(
+  "rateLimiter({ path }) must be a string"
+);
 
-export const rLLimitErr: Error = new ErrorGen(
+export const rLLimitErr: Error = new Error(
   "rateLimiter({ limit }) must be a positive int"
 );
 
-export const rLExpErr: Error = new ErrorGen(
+export const rLExpErr: Error = new Error(
   "rateLimiter({ expire }) must be a positive int"
 );
 
 // RATE_LIMITER_PROMISE ERRORS
-export const rLPUsernameErr: Error = new ErrorGen(
+export const rLPUsernameErr: Error = new Error(
   "there is no username, please set options.username"
 );
 
-export const rLPActionErr: Error = new ErrorGen(
+export const rLPActionErr: Error = new Error(
   "there is no action, please set options.action"
 );
 
-export const rLPLimitErr: Error = new ErrorGen(
+export const rLPLimitErr: Error = new Error(
   "rateLimiterPromise({ limit }) must be a positive int"
 );
 
-export const rLPExpErr: Error = new ErrorGen(
+export const rLPExpErr: Error = new Error(
   "rateLimiterPromise({ expire }) must be a positive int"
 );
 
